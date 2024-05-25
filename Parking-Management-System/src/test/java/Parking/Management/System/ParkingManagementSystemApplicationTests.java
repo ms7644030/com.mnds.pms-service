13aa482ccaa522e71755af355812beeb5df74166
Manish Kumar Singh <49101659+ms7644030@users.noreply.github.com>
2024-05-25 17:13:23 +0530

    initial commit

---
+++ Parking-Management-System/src/test/java/Parking/Management/System/ParkingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Parking.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
